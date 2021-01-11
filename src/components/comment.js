03684043479b8a530c328b84c42cc6af1df8e589
Muhammad Shafar <777uliahshafar@gmail.com>
2021-01-11 15:04:58 +0800

    revitalise staticman comment

--- src/components/comment.js
+++ src/components/comment.js
@@ -1,26 +1,17 @@
-import React from 'react'
-import s from './_comments.module.scss'
+import React from 'react';
+import s from './_comments.module.scss';
 
-const Comments = (props) => {
+const Comments = props => {
   return (
     <div>
       <form
         className={s.form}
         method="POST"
-        action="https://komentar-cepa.herokuapp.com/v2/entry/777uliahshafar/gatsbyBlogonline/master/comments"
+        action="https://komentar-saja.herokuapp.com/v2/entry/777uliahshafar/homeSaja/master/comments"
       >
-        <input
-          name="fields[slug]"
-          type="hidden"
-          value={props.slug}
-        />
+        <input name="fields[slug]" type="hidden" value={props.slug} />
         <input name="fields[name]" type="text" placeholder="Name" required />
-        <input
-          name="fields[email]"
-          type="email"
-          placeholder="Email"
-          required
-        />
+        <input name="fields[email]" type="email" placeholder="Email" required />
         <textarea name="fields[message]" placeholder="Comment" required />
         <button type="submit">Submit Comment</button>
       </form>
@@ -37,10 +28,10 @@ const Comments = (props) => {
           </div>
         ))
       ) : (
-          <p>No comments yet.</p>
-        )}
+        <p>No comments yet.</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
